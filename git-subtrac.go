81d016236880627ef4a5ce02fc4bfc6debb7d5d6
Avery Pennarun <apenwarr@gmail.com>
2019-11-26 22:26:46 -0500

    Fix error message when started from outside a git worktree.
    Fixes #6

--- git-subtrac.go
+++ git-subtrac.go
@@ -42,11 +42,6 @@ func main() {
 	verbose := getopt.BoolLong("verbose", 'v', "verbose mode")
 	getopt.Parse()
 
-	r, err := git.PlainOpen(*repodir)
-	if err != nil {
-		fatalf("git: %v: %v\n", repodir, err)
-	}
-
 	args := getopt.Args()
 	if len(args) < 1 {
 		usagef("no command specified.")
@@ -59,9 +54,17 @@ func main() {
 		debugf = func(fmt string, args ...interface{}) {}
 	}
 
-	c, err := NewCache(*repodir, r, *excludes, *autoexclude, debugf, infof)
-	if err != nil {
-		fatalf("NewCache: %v\n", err)
+	setupOrFatal := func() *Cache {
+		r, err := git.PlainOpen(*repodir)
+		if err != nil {
+			fatalf("git: %v: %v\n", *repodir, err)
+		}
+
+		c, err := NewCache(*repodir, r, *excludes, *autoexclude, debugf, infof)
+		if err != nil {
+			fatalf("NewCache: %v\n", err)
+		}
+		return c
 	}
 
 	switch args[0] {
@@ -69,6 +72,7 @@ func main() {
 		if len(args) != 1 {
 			usagef("command 'update' takes no arguments")
 		}
+		c := setupOrFatal()
 		err := c.UpdateBranchRefs()
 		if err != nil {
 			fatalf("%v\n", err)
@@ -77,6 +81,7 @@ func main() {
 		if len(args) != 2 {
 			usagef("command 'cid' takes exactly 1 argument")
 		}
+		c := setupOrFatal()
 		refname := args[1]
 		trac, err := c.TracByRef(refname)
 		if err != nil {
@@ -89,6 +94,7 @@ func main() {
 		if len(args) < 2 {
 			usagef("command 'dump' takes at least 1 argument")
 		}
+		c := setupOrFatal()
 		for _, refname := range args[1:] {
 			_, err := c.TracByRef(refname)
 			if err != nil {
